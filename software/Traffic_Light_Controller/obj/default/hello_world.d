3b4074ee1bea84b8ee793da3ad1b6420ba0f2db0
Kevin Tang <ktan404@UoA.auckland.ac.nz>
2019-08-24 16:57:45 +1200

    just a simple refactor in a ligma plane

---
+++ software/Traffic_Light_Controller/obj/default/hello_world.d
@@ -0,0 +1,34 @@
+obj/default/hello_world.o: hello_world.c \
+ ../Traffic_Light_Controller_bsp//HAL/inc/sys/alt_alarm.h \
+ ../Traffic_Light_Controller_bsp//HAL/inc/sys/alt_llist.h \
+ ../Traffic_Light_Controller_bsp//HAL/inc/alt_types.h \
+ ../Traffic_Light_Controller_bsp//HAL/inc/priv/alt_alarm.h \
+ ../Traffic_Light_Controller_bsp/system.h \
+ ../Traffic_Light_Controller_bsp/linker.h \
+ ../Traffic_Light_Controller_bsp//drivers/inc/altera_avalon_pio_regs.h \
+ ../Traffic_Light_Controller_bsp//HAL/inc/io.h \
+ ../Traffic_Light_Controller_bsp//HAL/inc/alt_types.h \
+ ../Traffic_Light_Controller_bsp//HAL/inc/sys/alt_irq.h \
+ ../Traffic_Light_Controller_bsp//HAL/inc/nios2.h
+
+../Traffic_Light_Controller_bsp//HAL/inc/sys/alt_alarm.h:
+
+../Traffic_Light_Controller_bsp//HAL/inc/sys/alt_llist.h:
+
+../Traffic_Light_Controller_bsp//HAL/inc/alt_types.h:
+
+../Traffic_Light_Controller_bsp//HAL/inc/priv/alt_alarm.h:
+
+../Traffic_Light_Controller_bsp/system.h:
+
+../Traffic_Light_Controller_bsp/linker.h:
+
+../Traffic_Light_Controller_bsp//drivers/inc/altera_avalon_pio_regs.h:
+
+../Traffic_Light_Controller_bsp//HAL/inc/io.h:
+
+../Traffic_Light_Controller_bsp//HAL/inc/alt_types.h:
+
+../Traffic_Light_Controller_bsp//HAL/inc/sys/alt_irq.h:
+
+../Traffic_Light_Controller_bsp//HAL/inc/nios2.h:
